94a60eb993d4b2d5b3e753fe262ec6fae13c30ea
Britney Smith <brit.smith7@gmail.com>
2018-09-10 19:29:53 -0400

    Added data model layer

---
+++ MemoryJournal/MemoryJournal/Entries.swift
@@ -0,0 +1,16 @@
+//
+//  Entries.swift
+//  MemoryJournal
+//
+//  Created by Britney Smith on 9/10/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+struct Entries {
+    let entries = [(title: "Entry Title 1", content: "content 1", date: "date 1"),
+                   (title: "Entry Title 2", content: "content 2", date: "date 2"),
+                   (title: "Entry Title 3", content: "content 3", date: "date 3")]
+
+}
---
+++ MemoryJournal/MemoryJournal/JournalEntry.swift
@@ -0,0 +1,22 @@
+//
+//  JournalEntry.swift
+//  MemoryJournal
+//
+//  Created by Britney Smith on 9/10/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+//Journal entries should have a title, content, and a date
+class JournalEntry: NSObject {
+    var title: String
+    var content: String
+    var date: String
+    
+    init(title: String, content: String, date: String) {
+        self.title = title
+        self.content = content
+        self.date = date
+    }
+    
+}
